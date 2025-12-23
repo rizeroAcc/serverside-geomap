9a40df889175628f7897655f9b77c7dea383eaa9
rizero <kirya123bu@bk.ru>
2025-12-23 19:47:18 +0300

    -made change password endpoint

--- src/main/kotlin/database/tables/ProjectUsersTable.kt
+++ src/main/kotlin/database/tables/ProjectUsersTable.kt
@@ -4,8 +4,8 @@ import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
 object ProjectUsersTable : Table("project_users") {
-    val projectId = uuid("project_id").references(ProjectTable.id, onUpdate = ReferenceOption.CASCADE)
-    val userPhone = varchar("user_phone",12)
+    val projectId = uuid("project_id").references(ProjectTable.id)
+    val userPhone = varchar("user_phone",12).references(UserTable.phone)
     val role = short("role")
 
     override val primaryKey = PrimaryKey(projectId,userPhone)
