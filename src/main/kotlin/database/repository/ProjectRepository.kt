6e3dcd64a038dd199ddd21f5da5b89e2ff5ec34a
rizero <kirya123bu@bk.ru>
2026-01-03 14:45:12 +0300

    -docs in ProjectRepository.kt

--- src/main/kotlin/database/repository/ProjectRepository.kt
+++ src/main/kotlin/database/repository/ProjectRepository.kt
@@ -6,8 +6,22 @@ import com.mapprjct.model.dto.Role
 import java.util.UUID
 
 interface ProjectRepository {
+    /**
+     * Select all uer projects and role in them
+     * */
     suspend fun getAllUserProjects(userPhone: String) : List<ProjectWithRole>
+    /**
+     * Create new project and insert record in ProjectUsersTable with Owner role
+     * */
     suspend fun insertProject(creatorPhone : String, projectName: String) : Project
+    /**
+     * Try to find project with ID
+     *
+     * @return the project if it exists, null otherwise
+     */
     suspend fun getProjectById(projectId : UUID) : Project?
+    /**
+     * Insert record in ProjectUsersTable and increment members count in project
+     */
     suspend fun addMemberToProject(userPhone : String, project: Project, role : Role)
 }
